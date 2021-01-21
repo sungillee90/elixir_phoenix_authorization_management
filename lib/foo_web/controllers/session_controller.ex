2932d5dc98afe1479144894959eecedd7919da14
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-21 14:33:49 -0600

    added basic authentification structure to the system

---
+++ lib/foo_web/controllers/session_controller.ex
@@ -0,0 +1,27 @@
+defmodule FooWeb.SessionController do
+  use FooWeb, :controller
+
+  alias Foo.Accounts
+
+  def new(conn, _) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"user" => %{"email" => email, "password" => password}}) do
+    case Accounts.authenticate_by_email_password(email, password) do
+#     authorized user "WELCOME"
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
+        |> redirect(to: "/")
+#        check your email/password and redirect
+      {:error, unauthorized} ->
+        conn
+        |> put_flash(:error, "Bad email/password")
+        |> redirect(to: session_path(conn, :new))
+    end
+  end
+
+end
